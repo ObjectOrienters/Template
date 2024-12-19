3461fdc15bce9189f87fa9e97b18285691ba1f77
Angela Salem <angel.salem.008@gmail.com>
2024-12-19 16:12:44 +0100

    f2

--- untitled/src/main/java/com/angela/Main.java
+++ untitled/src/main/java/com/angela/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 
      System.out.prin("mistake");
+        System.out.prin("mistake");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
